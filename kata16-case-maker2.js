ff06498495bf9c61564f1a34b56e1c5b046bd80d
Timothy Han <timothyh124@gmail.com>
2021-05-13 21:24:52 -0400

    added comments on kata16

--- kata16-case-maker2.js
+++ kata16-case-maker2.js
@@ -9,23 +9,32 @@ Precedence of each of the casing styles are as follows, values higher in the lis
 Our function should be able to handle all of these cases.
 For more information on casing styles, read Wikipedia's Special Case Styles for a list of various casing examples.*/
 
-const makeCase = function(input, type) {
+const VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']; // any constant that are globally true, can be defined at the very top of the program right after the imports. Use Uppercases for variable name
+
+const makeCase = function(input, type) { // Looks like type receives an array, then it should be types instead of type
   
   let initialInput = input.toLowerCase()
-  let output = initialInput;
+  let output = initialInput; // is there any reason why we are copying the value to the different variable?
   
+
+  // if type is already a array, why are we doing this?
+  // I am assuming that you are doing this because the argument type can be either string(containing a single value) or an array (containing multiple values)
+  // When you desing a function, it is not the best practice to receive an argument that can be something or something else (like string or array).
+  // the reason is because it makes functions to handle many different scenarios and it causes the function to be more complicated => more bugs and harder to maintain
+  // You should decide if you want to receive type as string or array.
   const typesInArray = [];  
   (Array.isArray(type)) ? type.forEach(type => typesInArray.push(type)) : typesInArray.push(type);
 
-  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
   
+  // below is functions and functions without input? I am confused
+  // I see why you created duplicated called `output`. I would encourage you to make below functions to receive inputs instead of directly accessing the exsiting variables.
   const camelCase = () => initialInput.split(' ').map((word, index) => (index > 0) ? `${word[0].toUpperCase()}${word.slice(1)}` : word).join('');
   const pascalCase = () => initialInput.split(' ').map(word => `${word[0].toUpperCase()}${word.slice(1)}`).join('');
   const snakeCase = () => initialInput.split(' ').map((word, index, array) => (index < array.length-1) ? word + '_' : word).join('');
   const kebabCase = () => initialInput.split(' ').map((word, index, array) => (index < array.length-1) ? word + '-' : word).join('');
   const titleCase = () => initialInput.split(' ').map(word => `${word[0].toUpperCase()}${word.slice(1)}`).join(' ');
-  const vowelCase = () => output.split('').map(character => (vowels.includes(character)) ? character.toUpperCase(): character).join('');
-  const consonantCase = () => output.toUpperCase().split('').map(character => (vowels.includes(character)) ? character.toLowerCase(): character).join('');
+  const vowelCase = () => output.split('').map(character => (VOWELS.includes(character)) ? character.toUpperCase(): character).join('');
+  const consonantCase = () => output.toUpperCase().split('').map(character => (VOWELS.includes(character)) ? character.toLowerCase(): character).join('');
   const upperCase = () => output.toUpperCase();
   const lowerCase = () => output.toLowerCase();
 
@@ -39,10 +48,14 @@ const makeCase = function(input, type) {
       case 'vowel': output = vowelCase(); break;
       case 'consonant': output = consonantCase(); break;
       case 'upper': output = upperCase(); break;
-      case 'lower': output = lowerCase(); break;
+      case 'lower': output = lowerCase(); break; 
+      // missing a default case
+      // also in this case, the output will only have the latest conversion result.
+      // e.g. if there are multiple types passed, [camel, upper], then at the end the output will have upper conversion only. camel case conversion will be lost.
     }
   }
     //firstClassTypes should be entered at index of 0, and SecondClassTypes come after.
+    // from here to return output, I dont understand what is coing on. 
     const firstClassTypes = ['camel', 'pascal', 'snake', 'kebab', 'title'];
     const secondClassTypes = ['vowel', 'consonant', 'upper', 'lower' ];
     const allClasses = firstClassTypes.concat(secondClassTypes);
